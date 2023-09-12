fc3621eae2d7ee92c2e4c5acc28c31486e637938
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2023-09-12 11:27:49 +0200

    Update index.php

--- aethoria/index.php
+++ aethoria/index.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "<div> <p>Titel: " . $row["title"]. "</p> <p>Notiz: " . $row["note"]. "</p> <p>Port:" . $row["port"]. "</p> </div>";
+    echo "<div> <a href=\"index.php?delete=" . $row["id"] . "\" >Delete</href> <p>Titel: " . $row["title"]. "</p> <p>Notiz: " . $row["note"]. "</p> <p>Port:" . $row["port"]. "</p> </div>";
   }
 } else {
   echo "0 results";
