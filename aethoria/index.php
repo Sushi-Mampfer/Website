643fd4d393cfd4954bdbc1992f64d0950aaa4d81
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2023-09-12 11:06:33 +0200

    Update index.php

--- aethoria/index.php
+++ aethoria/index.php
@@ -11,15 +11,15 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
 $sql = "SELECT id, title, note, port FROM notes";
-$result = mysqli_query($conn, $sql);
-echo "hi"
-if (mysqli_num_rows($result) > 0) {
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
   // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-  	echo "<div> <a href=\"index.php?delete=" . $row["id"] ."\">Delete</a> <p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "</div>";
+  while($row = $result->fetch_assoc()) {
+    echo "<div> <a href=\"index.php?delete=" . $row["id"] ."\">Delete</a> <p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "</div>";
   }
 } else {
   echo "0 results";
 }
-mysqli_close($conn);
+$conn->close();
 ?>
