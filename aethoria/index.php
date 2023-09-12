e52dff9e1acff8bdc10500bb2ce168e131ab3d65
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2023-09-12 11:03:33 +0200

    Update index.php

--- aethoria/index.php
+++ aethoria/index.php
@@ -13,11 +13,11 @@ if ($conn->connect_error) {
 if 
 $sql = "SELECT id, title, note, port FROM notes";
 $result = mysqli_query($conn, $sql);
-
+echo "hi"
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-  	echo "<div> <p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "</div>";
+  	echo "<div> <a href=\"index.php?delete=" . $row["id"] ."\">Delete</a> <p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "</div>";
   }
 } else {
   echo "0 results";
