cf64bf1bc863cba492b8dcb0b3bbd4dea9bf4df9
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2023-09-12 11:04:10 +0200

    Update index.php

--- aethoria/index.php
+++ aethoria/index.php
@@ -3,14 +3,13 @@ $servername = "localhost";
 $username = "aethoria";
 $password = "Test1234!";
 $dbname = "aethoria";
-
+echo "hi"
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
-if 
+} 
 $sql = "SELECT id, title, note, port FROM notes";
 $result = mysqli_query($conn, $sql);
 echo "hi"
