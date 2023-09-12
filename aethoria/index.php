4a5ac27a8498d360897f003d006b140de3b2a4f4
Sushi-Mampfer <84100259+Sushi-Mampfer@users.noreply.github.com>
2023-09-12 11:07:54 +0200

    Update index.php

--- aethoria/index.php
+++ aethoria/index.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<div><p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "</div>";
+    echo "<div><p>Title: " . $row["title"]. "</p> <p>Note: " . $row["note"]. "</p> <p>Port: " . $row["port"]. "<p/> </div>";
   }
 } else {
   echo "0 results";
