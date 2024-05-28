77078f50acd323e8c8259651055afb13f169cfc2
Sushi-Mampfer <tim.schwyzer@gmx.ch>
2024-05-28 19:17:18 +0200

    modified:   1.php

--- 1.php
+++ 1.php
@@ -1,5 +1,5 @@
 <?php
-    if (!$_POST["passwd"] == "sdpm") {
+    if (!($_POST["passwd"] == "sdpm")) {
         echo('<form action="1.php" method="post"><label for="passwd">Passwort: </label><input type="passwd" name="passwd" id="passwd"></form>');
         exit();
     }
