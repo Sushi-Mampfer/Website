e62d8020728f2e2018813d3af7224e2ea056762d
Sushi-Mampfer <tim.schwyzer@gmx.ch>
2024-05-30 13:06:08 +0200

    new file:   4.html 	modified:   final.php

--- final.php
+++ final.php
@@ -1,6 +1,6 @@
 <?php
-    if (!($_POST["passwd"] == "aekb")) {
-        echo('<form action="final.php" method="post"><label for="passwd">Afangsbuechstabe vo de Lösige(4): </label><input type="passwd" name="passwd" id="passwd"></form>');
+    if (!($_POST["passwd"] == "aekg")) {
+        echo('<form action="final.php" method="post"><label for="passwd">Afangsbuechstabe vo de Lösige(4, chlibueschstabe): </label><input type="passwd" name="passwd" id="passwd"></form>');
         exit();
     } else {
         echo("Lueg nomol wo du agfange hesch ;)");
