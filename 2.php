977b7ebd3105d169a821c046ccb01402d6b30f56
Sushi-Mampfer <tim.schwyzer@gmx.ch>
2024-06-13 19:45:04 +0200

    deleted:    2.php 	deleted:    4.html 	modified:   css/style.css 	deleted:    final.php 	modified:   index.html

--- 2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (!($_POST["passwd"] == "sdpm")) {
-        echo('<form action="1.php" method="post"><label for="passwd">Passwort: </label><input type="passwd" name="passwd" id="passwd"></form>');
-        exit();
-    } else {
-        echo("eltere bad");
-    }
-?>
--- final.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (!($_POST["passwd"] == "aekg")) {
-        echo('<form action="final.php" method="post"><label for="passwd">Afangsbuechstabe vo de Lösige(4, chlibueschstabe): </label><input type="passwd" name="passwd" id="passwd"></form>');
-        exit();
-    } else {
-        echo("Lueg nomol wo du agfange hesch ;)");
-    }
-?>
